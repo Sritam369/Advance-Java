55403057062dc5d2fc5e7155bda9f4d43b5f4cae
Sritam369 <chakrabarttysritam@gmail.com>
2026-01-10 12:36:47 +0530

    updating table data

--- AdvanceJava/src/com/pack1/JdbcPro2.java
+++ AdvanceJava/src/com/pack1/JdbcPro2.java
@@ -4,15 +4,32 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.SQLIntegrityConstraintViolationException;
 import java.sql.Statement;
+import java.util.Scanner;
 
 public class JdbcPro2 {
    String driver = "oracle.jdbc.OracleDriver";
    String dbUrl= "jdbc:oracle:thin:@localhost:1521:orcl";
    String dbUname="sritam";
    String dbPwd="wolf";
+   Scanner sc = new Scanner(System.in);
    String query="select * from employee";
+   String sqlQuery2="insert into employee values ('103','subrat','naik',45000,'hyd')";
+   String sqlQuery3="delete from employee where eid='103'";
+   String sqlQuery4="update employee set esal=35000 where EID='101'";
    
+   Connection connect() {
+	   Connection con=null;
+	   try {
+		   Class.forName(driver);
+		    con = DriverManager.getConnection(dbUrl, dbUname, dbPwd);
+	   }
+	   catch(Exception e) {
+		   e.printStackTrace();
+	   }
+	   return con;
+   }
    void getEmployeeDetails() {
 	   try {
 	   Class.forName(driver); // THIS WILL THROW CLASSNOTFOUNDEXCEPTION
@@ -38,8 +55,93 @@ public class JdbcPro2 {
 		  }
 	   }
    }
-   void main() {
-	   new JdbcPro2().getEmployeeDetails();
+   
+   void insertEmpData() {
+	   try {
+		  
+		   Connection con = connect();
+		   Statement stm = con.createStatement();
+		   int rowCount = stm.executeUpdate(sqlQuery2);
+		   if(rowCount==0) {
+			   IO.println("Data not inserted");
+		   }
+		   else {
+			   IO.println(rowCount+" row inserted successfully");
+			   getEmployeeDetails();
+		   }
+	   }
+	   catch(Exception e) {
+		   if(e instanceof SQLIntegrityConstraintViolationException) {
+			   IO.println("Duplicate EMPID'S are not allowed");
+		   }
+		   else {
+		   e.printStackTrace();
+		   }
+	   }
    }
-}
+   		
+   void deleteEmpData() {
+	   try {
+		   Connection con=connect();
+		   Statement stm = con.createStatement();
+		   int rowCount=stm.executeUpdate(sqlQuery3);
+		   if(rowCount==0) {
+			   IO.println("there is no data with given empid");
+		   }
+		   else {
+			   IO.println("data with given empid deleted successfully ");
+			   IO.println("Do you want to view the emp table data [y/n]");
+			   char choice=sc.nextLine().charAt(0);
+			   if(choice=='y'|| choice=='Y') {
+				   getEmployeeDetails();
+			   }
+			   else {
+				   System.exit(0);
+			   }
+		   }
+	   }
+	   catch(Exception e) {
+		   e.printStackTrace();
+	   }
+   }
+   
+   void updateEmpDetails() {
+	
+	   try {
+	   Connection con=connect();
+	 
+	   Statement stm = con.createStatement();
+	  
+	   int rowCount=stm.executeUpdate(sqlQuery4);
+	   
+	   IO.println(rowCount);
+	   if(rowCount==0) {
+		   IO.println("No data found with given empid");
+	   }
+	   else {
+		   IO.println("data updated successfully");
+		   IO.println("Do you want to view the emp table data [y/n]");
+		   char choice=sc.nextLine().charAt(0);
+		   if(choice=='y'|| choice=='Y') {
+			   getEmployeeDetails();
+		   }
+		   else {
+			   System.exit(0);
+		   }
+	   }
+	   }
+	   catch(Exception e) {
+		   e.printStackTrace();
+	   }
+   }
+   
+   void main() {
+	   JdbcPro2 jdbc= new JdbcPro2();
+	   //jdbc.insertEmpData();
+	   //jdbc.deleteEmpData();
+	   jdbc.updateEmpDetails();
+   }
+	   
+   }
+
 
